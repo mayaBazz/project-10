29c9bdc647be5d98b66be11cd1adb4388640b8bf
mayaBazz <87096736+mayaBazz@users.noreply.github.com>
2021-09-20 18:56:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var sea;
+var ship;
+
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+  sea = createSprite(400,200,400)
+sea.addImage(seaImg);
+  
+  
+  ship = createSprite(200,300,50,50);
+   ship.addAnimation("shipImg", shipImg1);
+   ship.scale=0.2
+
+
+
+ 
+}
+
+
+function draw() {
+ 
+  background("blue");
+ 
+  if(sea.x < 0){
+    sea.x = sea.width/2;
+  }
+  sea.velocityX=-5
+  
+ drawSprites()
+}
